a23891c6b79b23331bee98a8b3b05783a3d92e19
wickker <choydianchun@gmail.com>
2020-06-11 23:37:45 +0800

    able to write new instructor to database

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,6 +1,14 @@
 module.exports = (db) => {
   const newInstructor = (request, response) => {
     console.log(request.body);
+    let name = request.body.name;
+    let image = request.body.image;
+    let about = request.body.about;
+    let checked = request.body.checkedState;
+    let callback = () => {
+      response.redirect("/instructors");
+    }
+    db.instructors.writeNewInstructor(callback, name, image, about, checked);
   }
 
 
--- models/instructors.js
+++ models/instructors.js
@@ -1,7 +1,29 @@
 module.exports = (pool) => {
 
+  const writeNewInstructor = async (callback, name, image, about, checked) => {
+    let queryText = `insert into instructors (name, image, about, is_delete) values ('${name}', '${image}', '${about}', false) returning *`;
+    await pool.query(queryText).then(async (result) => {
+      console.log(result.rows[0]);
+      let instructorId = result.rows[0].id;
+      console.log(instructorId);
+      for (const key in checked) {
+        let obj = checked[key]["current"];
+        for (const prop in obj) {
+          if (obj[prop]) {
+            let sessionId = parseInt(prop);
+            let classId = parseInt(key);
+            queryText = `insert into instructors_classes (instructor_id, session_id, class_id) values (${instructorId}, ${sessionId}, ${classId}) returning *`;
+            await pool.query(queryText).then(async (result) => {
+              console.log(result.rows[0]);
+            });
+          }
+        }
+      }
+    });
+    callback();
+  }
 
   return {
-   
+   writeNewInstructor,
   };
 };
